1c75ec45b003c5ff5a00b7f186e763790673d169
tolupatrick004 <princericky2000@yahoo.com>
2018-08-01 15:07:39 +0100

    Add reducers/actions/store

---
+++ src/actions/auth.js
@@ -0,0 +1,26 @@
+import { createAction } from 'redux-actions';
+import axios from 'axios';
+
+const BASE_URL = 'http://ivotenaija.herokuapp.com/api/v1/';
+
+const LOGIN_PENDING = 'LOGIN_PENDING';
+const LOGIN_SUCCESS ='LOGIN_SUCCESS';
+const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const loginPending = createAction(LOGIN_PENDING)
+export const loginSuccess = createAction(LOGIN_SUCCESS)
+export const loginFailure = createAction(LOGIN_FAILURE)
+
+export const login = ({ email, password }) => (dispatch) => {
+  dispatch(loginPending());
+  return axios.post(BASE_URL + 'users/login', { email, password })
+    .then(result => {
+      dispatch(loginSuccess(result))
+    }, err => {
+      dispatch(loginFailure(err.message))
+    })
+    .catch(err => {
+      dispatch(loginFailure(err.message))
+    })
+}
+
