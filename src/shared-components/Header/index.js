7ea5ae7f53645928db35e4dc8ddaa810628545df
tolupatrick004 <princericky2000@yahoo.com>
2018-08-18 01:32:46 +0100

    fix the back button bug on moving from sign up to log in page

--- src/shared-components/Header/index.js
+++ src/shared-components/Header/index.js
@@ -4,24 +4,28 @@ import { StyleSheet } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { VIEWS } from '../../components/Home/constants';
 
-const HeaderTab = ({menu, back, title, setActive}) => 
-  <Header style={styles.header} hasTabs>
-    <Left>
-      { menu && 
-        <Button transparent onPress={() => Actions.drawerOpen()}>
-          <Icon name='menu' type='MaterialCommunityIcons' style={{color: '#3F3F3F', marginLeft: 10 }}/> 
+const HeaderTab = ({menu, back, title, setActive}) => {
+  const backfn = (view) => setActive ? setActive(view) : Actions.pop();
+  return (
+    <Header style={styles.header} hasTabs>
+      <Left>
+        { menu && 
+          <Button transparent onPress={() => Actions.drawerOpen()}>
+            <Icon name='menu' type='MaterialCommunityIcons' style={{color: '#3F3F3F', marginLeft: 10 }}/> 
+          </Button> }
+        { back && 
+        <Button transparent onPress={() => backfn(VIEWS.feed)}>
+          <Icon name='chevron-left' type='MaterialCommunityIcons' style={{color: '#3F3F3F', marginLeft: 10 }}/>
         </Button> }
-      { back && 
-      <Button transparent onPress={() => setActive(VIEWS.feed)}>
-         <Icon name='chevron-left' type='MaterialCommunityIcons' style={{color: '#3F3F3F', marginLeft: 10 }}/>
-      </Button> }
-    </Left>
-    <Body>
-      <Title style={styles.title}>{title}</Title>
-    </Body>
-    <Right>
-    </Right>
-  </Header>;
+      </Left>
+      <Body>
+        <Title style={styles.title}>{title}</Title>
+      </Body>
+      <Right>
+      </Right>
+    </Header>
+  );
+}
 
 export default HeaderTab;
 
