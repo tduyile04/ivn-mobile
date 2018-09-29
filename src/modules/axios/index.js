f666efef3fd093d1c66a637df41a97ef6fe9426d
Sunil Karmur <sunilahir880@gmail.com>
2018-09-29 09:05:49 +0530

    authenticate handle

--- src/modules/axios/index.js
+++ src/modules/axios/index.js
@@ -1,10 +1,45 @@
 import axios from 'axios'
+import { Alert } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { get, remove } from '../../modules/cache';
+
+const logout = ()=>{
+
+  remove('token')
+
+    return Actions.login();
+}
 
 export default (token) => {
-  return axios.create({
-    baseURL: 'https://ivotenaija.herokuapp.com/',
-    headers: {
-      'Authorization': `${token}`
-    }
-  })
+
+  let instance = axios.create({
+        baseURL: 'https://ivotenaija.herokuapp.com/',
+        headers: {
+            'Authorization': `${token}`
+        }
+    })
+
+    instance.interceptors.response.use(function (response) {
+        // Do something with response data
+        return response;
+    }, function (error) {
+        const sources = error.response.data;
+        // Do something with response error
+        if(sources.status.code == 403){
+          Alert.alert(
+              'UnAuthorize',
+              sources.error.message,
+              [
+                  {text:'ok', onPress:()=>logout()}
+              ]
+          )
+        }
+        console.log(sources.error.message)
+
+        return Promise.reject(error);
+    });
+
+  return instance;
 }
+
+
